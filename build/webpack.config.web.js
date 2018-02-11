496449031cb792308bd99f0269c41669739fa32e
Clément Ollivier <clement.o2p@gmail.com>
2018-02-11 12:29:40 +0100

    Add dev server with HMR for web environment

--- build/webpack.config.web.js
+++ build/webpack.config.web.js
@@ -1,10 +1,15 @@
 const utils = require('./utils')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const DefinePlugin = require('webpack/lib/DefinePlugin')
+const webpack = require('webpack')
 
 module.exports = {
   target: 'web',
   entry: utils.srcPath('entry.web.js'),
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
   output: {
     filename: 'app.bundle.js',
     path: utils.distPath('web')
@@ -29,6 +34,8 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: utils.srcPath('index.html')
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
